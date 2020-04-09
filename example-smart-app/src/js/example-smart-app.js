04a59ee4c15362492ee7e385284179684df7a776
John <dobiedevelopments@gmail.com>
2020-04-09 05:39:54 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -35,6 +35,8 @@
           var byCodes = smart.byCodes(obv, "code");
           var gender = patient.gender;
 
+          console.log(patient);
+
           var fname = "";
           var lname = "";
 
