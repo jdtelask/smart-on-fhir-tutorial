64ed94ba48a73d4df2bdfdda05a89ac5ba580028
John <dobiedevelopments@gmail.com>
2020-04-09 05:19:41 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -67,7 +67,7 @@
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
           console.log("hello");
-          console.log(byCodes("11506-3"));
+          console.log(byCodes("55284-4"));
 
           ret.resolve(p);
         });
